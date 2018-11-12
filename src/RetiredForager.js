c46cfb669de91a40b18a622f50a773cf7be3c199
Tyler Nguyen <tydnguyen@ucdavis.edu>
2018-11-12 11:28:41 -0800

    finished beesbeesbees

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,14 @@
 var RetiredForagerBee = function() {
+    ForagerBee.call(this, arguments);
+    this.age = 40;
+    this.job = 'gamble';
+    this.canFly = false;
+    this.color = 'grey';
+
 };
 
+RetiredForagerBee.prototype = Object.create(ForagerBee.prototype);
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
+RetiredForagerBee.prototype.gamble = function (treasure) {
+  this.treasureChest.push(treasure);
+};
